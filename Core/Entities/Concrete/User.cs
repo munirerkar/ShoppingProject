0c415d96c72d6b7c37084733a745b1787eacef0d
Münir Erkar <manoerkar347@gmail.com>
2023-01-16 16:01:39 +0300

    User

--- Core/Entities/Concrete/User.cs
+++ Core/Entities/Concrete/User.cs
@@ -9,10 +9,13 @@ namespace Core.Entities.Concrete
 {
     public class User : IEntity
     {
-        public int Id { get; set; }
+        public int UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public bool EmailConfirmed { get; set; }
         public byte[] PasswordSalt { get; set; }
         public byte[] PasswordHash { get; set; }
         public bool Status { get; set; }
---
+++ DataAccess/Mapping/UserMap.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities.Concrete;
+
+namespace DataAccess.Mapping
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+
+            // Indexes for "normalized" username and email, to allow efficient lookups
+            builder.HasIndex(u => u.NormalizedUserName).HasName("UserNameIndex").IsUnique();
+            builder.HasIndex(u => u.NormalizedEmail).HasName("EmailIndex");
+
+            // Maps to the AspNetUsers table
+            builder.ToTable("AspNetUsers");
+
+            // A concurrency token for use with the optimistic concurrency checking
+            builder.Property(u => u.ConcurrencyStamp).IsConcurrencyToken();
+
+            // Limit the size of columns to use efficient database types
+            builder.Property(u => u.UserName).HasMaxLength(256);
+            builder.Property(u => u.NormalizedUserName).HasMaxLength(256);
+            builder.Property(u => u.Email).HasMaxLength(256);
+            builder.Property(u => u.NormalizedEmail).HasMaxLength(256);
+
+            // The relationships between User and other entity types
+            // Note that these relationships are configured with no navigation properties
+
+            // Each User can have many UserClaims
+            builder.HasMany<User>().WithOne().HasForeignKey(uc => uc.UserId).IsRequired();
+
+            // Each User can have many UserLogins
+            builder.HasMany<User>().WithOne().HasForeignKey(ul => ul.UserId).IsRequired();
+
+            // Each User can have many UserTokens
+            builder.HasMany<User>().WithOne().HasForeignKey(ut => ut.UserId).IsRequired();
+
+            // Each User can have many entries in the UserRole join table
+            builder.HasMany<User>().WithOne().HasForeignKey(ur => ur.UserId).IsRequired();
+
+            var superadmin = new User
+            {
+                Email = "superadmin@gmail.com",
+                PhoneNumber = "+905439999999",
+                FirstName = "Mano",
+                LastName = "Ekr",
+                PhoneNumberConfirmed = true,
+                EmailConfirmed = true,
+            };
+
+            var admin = new User
+            {
+                Email = "superadmin@gmail.com",
+                PhoneNumber = "+905439999999",
+                FirstName = "Mano",
+                LastName = "Ekr",
+                PhoneNumberConfirmed = true,
+                EmailConfirmed = true,
+            };
+            builder.HasData(superadmin, admin);
+        }
+    }
+}
